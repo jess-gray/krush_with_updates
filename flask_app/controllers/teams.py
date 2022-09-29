876e4a93610ef5e3fb2f86886f8b23a6286d7798
jess_gray <jac.cooper32@gmail.com>
2022-09-28 22:59:29 -0400

    Working adding viewing one team info

--- flask_app/controllers/teams.py
+++ flask_app/controllers/teams.py
@@ -16,3 +16,16 @@ def team_info():
     all_teams = Team.get_all()
     print(all_teams, 'OOOOO')
     return render_template('teams.html', all_the_teams = all_teams)
+
+
+@app.route('/team/<int:id>') #this is to view one teams players
+def view_post(id):
+    data = { 
+        'id' : id
+    }
+    a_player = Player.get_one_with_team(data) #this doesn't need to be looped through bc it is a dictionary not a list.
+    all_players = Player.get_all_with_team() #looping through bc it is returning a list on the class method
+    print(all_players, '******')
+    return render_template('read_one.html', one_player = a_player,  all_the_players = all_players)
+
+    #Need to add link on teams html and create view one team html. Also need to add a player to test it is working 
--- flask_app/models/player.py
+++ flask_app/models/player.py
@@ -14,3 +14,41 @@ class Player:
         self.created_at = data['created_at']
         self.udpated_at = data['updated_at']
         self.owner = None #this is to associate the other table
+
+
+    @classmethod  #this is to show all players with one team 
+    def get_all_with_team(cls):
+        query = 'SELECT * FROM players JOIN teams ON players.team_id = team.id'
+        results = connectToMySQL('krush_project').query_db(query)
+        all_players = []
+        for row in results:
+            one_player = cls(row)
+            team_data = {
+                'id': row['users.id'],
+                'team_name' :row['team_name'],
+                'head_coach' :row['head_coach'],
+                'assistant_coach':row['assistant_coach'],
+                'created_at' : row['users.created_at'],
+                'updated_at' : row['users.updated_at']
+            }
+            team_obj = team.Team(team_data)
+            one_player.owner = team_obj
+            all_players.append(one_player)
+        return all_players
+    
+    @classmethod
+    def get_one_with_team(cls, data): #to get one user and one post
+        query = 'SELECT * FROM players JOIN teams ON players.user_id = teams.id WHERE players.id = %(id)s;'
+        results = connectToMySQL('krush_project').query_db(query, data)
+        one_player = cls(results[0])
+        user_data = {
+                'id': results[0]['users.id'],
+                'team_name' : results[0]['team_name'],
+                'head_coach': results[0]['head_coach'],
+                'assistant_coach': results[0]['assistant_coach'],
+                'created_at' : results[0]['users.created_at'],
+                'updated_at' : results[0]['users.updated_at']
+            }
+        one_player.owner = team.Team(user_data)    
+        print(results)
+        return one_player
